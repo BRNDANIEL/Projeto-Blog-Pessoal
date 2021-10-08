5eb72d638409bef527e328ec664c54c28914cc97
Bruno Daniel <brnvalero@gmail.com>
2021-10-07 22:27:09 -0300

    [atualizando blog]

--- Front-end/BlogBruno/src/app/login/login.component.ts
+++ Front-end/BlogBruno/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin= new UserLogin
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService 
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar(){
+this.auth.entrar(this.userLogin).subscribe((resp:UserLogin)=>{
+  this.userLogin=resp
+})
+
   }
 
 }
